7408de0944fb331f1003831479754510d23ca26f
Zoe <zoeinspired1609@gmail.com>
2026-05-28 13:27:28 +0100

    Multiplication Table of 7

---
+++ MultiplicationTable.java
@@ -0,0 +1,45 @@
+//public class MultiplicationTable{
+//    public static void main(String[] args){
+//    
+//    
+//  for(int number = 1; number <= 12; number++){
+//  
+//  System.out.println(" 7 " + " x " + number + " = " + number*7); 
+//  }  
+//
+//}
+//}
+
+//public class MultiplicationTable{
+//    public static void main(String[] args){
+//    
+//    int number = 1;
+//    
+//  while
+//  (number <= 12){
+//  
+//  number = number +1;
+//System.out.println(" 7 " + " x " + number + " = " + number*7); 
+//  }
+//
+//}
+//}
+//
+// DO WHILE LOOP
+
+public class MultiplicationTable{
+    public static void main(String[] args){
+    
+    int number = 1;
+    
+ do{
+ System.out.println(" 7 " + " x " + number + " = " + number*7);
+ 
+ number++;
+ }
+while
+(number <= 12);
+
+}
+}
+
